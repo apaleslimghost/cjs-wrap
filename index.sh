1033905233ce8d20020f00fd5546b1b037fce354
Matt Brennan <mattyb1000@gmail.com>
2016-12-20 19:36:50 +0000

    wrap

--- index.sh
+++ index.sh
@@ -4,18 +4,7 @@ readonly NAME="$1"
 readonly FILE="${2:-/dev/stdin}"
 
 cat <<EOF
-var module = {};
-Object.defineProperty(module, 'exports', {
-	set: function(value) {
-		window["$NAME"] = value;
-	},
-
-	get: function() {
-		return window["$NAME"];
-	}
-});
-
-var exports = module.exports = {};
-
+(function(module, exports = module.exports) {
 $(< "$FILE")
+})({ set exports(value) { window["$NAME"] = value }, get exports() { return window["$NAME"] } });
 EOF

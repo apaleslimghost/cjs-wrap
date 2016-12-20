cf53d8dfd06570be8b1d44857086ff573eae083d
Matt Brennan <mattyb1000@gmail.com>
2016-12-20 19:45:39 +0000

    init exports

--- index.sh
+++ index.sh
@@ -4,7 +4,7 @@ readonly NAME="$1"
 readonly FILE="${2:-/dev/stdin}"
 
 cat <<EOF
-(function(module, exports = module.exports) {
+(function(module, exports = module.exports = {}) {
 $(< "$FILE")
 })({ set exports(value) { window["$NAME"] = value }, get exports() { return window["$NAME"] } });
 EOF
